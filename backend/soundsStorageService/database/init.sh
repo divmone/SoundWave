4f86dea24edb54f2c6506a0d738811674c58e2a7
divmone <dimasston99@gmail.com>
2026-03-25 14:37:32 +0300

    fixes

--- backend/soundsStorageService/database/init.sh
+++ backend/soundsStorageService/database/init.sh
@@ -20,8 +20,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "soundwaveSounds" <<-EOSQ
     );
 
     CREATE TABLE IF NOT EXISTS tags (
-        id   BIGSERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL UNIQUE
+        id         BIGSERIAL PRIMARY KEY,
+        name       VARCHAR(100) NOT NULL UNIQUE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
     CREATE TABLE IF NOT EXISTS products (
