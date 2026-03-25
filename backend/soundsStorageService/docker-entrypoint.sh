bb871d3445361913b95dbc798befe31edad64688
IdkWhatToWriteHereOhManItWasAlreadyUsed <dimoon.amid05@gmail.com>
2026-03-25 09:42:00 +0300

    Наладил работу всего в докере

--- backend/soundsStorageService/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export DB_HOST="${DB_HOST:-postgres}"
-export DB_USER="${DB_USER:-postgres}"
-export DB_PASSWORD="${DB_PASSWORD:-postgres}"
-export DB_NAME="${DB_NAME:-soundwaveSounds}"
-
-envsubst < config/app/config.json.template > config/app/config.json
-
-exec ./SoundsStorageService
