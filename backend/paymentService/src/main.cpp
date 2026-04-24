cfcbacbfa7f0eea71ae8d4b0af07854030cf1cb9
IdkWhatToWriteHereOhManItWasAlreadyUsed <dimoon.amid05@gmail.com>
2026-04-24 23:59:25 +0300

    Отмена userver. Опять. Снова.

--- backend/paymentService/src/main.cpp
+++ backend/paymentService/src/main.cpp
@@ -1,33 +1,9 @@
-#include <userver/clients/dns/component.hpp>
-#include <userver/clients/http/component_list.hpp>
-#include <userver/components/component.hpp>
-#include <userver/components/component_list.hpp>
-#include <userver/components/minimal_server_component_list.hpp>
-#include <userver/congestion_control/component.hpp>
-#include <userver/server/handlers/ping.hpp>
-#include <userver/server/handlers/tests_control.hpp>
-#include <userver/testsuite/testsuite_support.hpp>
+//
+// Created by dmitry on 24.04.2026.
+//
 
-#include <userver/storages/postgres/component.hpp> 
 
-#include <userver/utils/daemon_run.hpp>
-
-#include <hello.hpp>
-#include <hello_postgres.hpp> 
-
-int main(int argc, char* argv[]) {
-    auto component_list =
-        userver::components::MinimalServerComponentList()
-            .Append<userver::server::handlers::Ping>()
-            .Append<userver::components::TestsuiteSupport>()
-            .AppendComponentList(userver::clients::http::ComponentList())
-            .Append<userver::clients::dns::Component>()
-            .Append<userver::server::handlers::TestsControl>()
-            .Append<userver::congestion_control::Component>()
-            .Append<paymentService::Hello>()
-            .Append<userver::components::Postgres>("postgres-db-1")
-            .Append<paymentService::HelloPostgres>()
-        ;
-
-    return userver::utils::DaemonMain(argc, argv, component_list);
+int main()
+{
+    return 0;
 }
